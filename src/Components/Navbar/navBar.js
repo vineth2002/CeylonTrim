fd5762f7ad0ce2254a9aca34a91fe50f5806d4e2
vineth2002 <130199275+vineth2002@users.noreply.github.com>
2024-10-08 10:05:40 +0530

    Ceylontrim Navbar done

---
+++ src/Components/Navbar/navBar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo 2.png'
+import search_icon_light from '../../assets/search-w.png'
+import search_icon_dark from '../../assets/search-b.png'
+import toggle_light from '../../assets/day.png'
+import toggle_dark from '../../assets/night.png'
+const Navbar=()=> {
+    return(
+        <div className='Navbar'>
+            <img src={logo} className='logo' alt='logo'/>
+            <ul className='options'>
+                <li>Home</li>
+                <li>Services</li>
+                <li>Book Now</li>
+                <li>Sign In</li>
+            </ul>
+            <div className='Search-box'> 
+                <input type='text' placeholder='Search' id=''></input>
+                <img src={search_icon_light} className='searchIcon' alt='Search-icon'/>
+            </div>
+            <img src={toggle_light} className='toggleIcon' alt='search-icon'/>
+            </div>
+    )
+            
+}
+export default Navbar;
+
